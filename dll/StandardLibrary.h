d83dbead29d83d2038d7dbc6c4969c284f6f7bea
switchkovskiy-maker <switchkovskiy@gmail.com>
2025-10-26 00:39:42 +0300

    Реализация stdLibrary DLL

---
+++ dll/StandardLibrary.h
@@ -0,0 +1,17 @@
+#ifndef StandardLibraryH
+#define StandardLibraryH
+
+#include "ComponentLibrary.h"
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
+
+extern "C" {
+	DLL_EXPORT bool __stdcall RegisterStandardLibrary(TLibraryManager* libraryManager);
+}
+
+#endif
